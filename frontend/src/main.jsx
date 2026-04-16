8db719cfa85d98cb018df9170bdd367855825931
akashwaradeofficial@gmail.com <Akash Warade>
2026-04-16 21:05:24 +0530

    spell correct

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRouter from './Router/AppRouter.jsx'
+import AppRouter from './router/AppRouter'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
   import { ToastContainer, toast } from 'react-toastify';
 const queryClient = new QueryClient()
