07e12f4daf7ff903b6e36ea447519709dc1ec66e
akashwaradeofficial@gmail.com <Akash Warade>
2026-04-16 21:53:49 +0530

    mongo fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const connectDB=async()=>{
  try {
-    let res=await mongoose.connect("mongodb://localhost:27017/auth-project")
+    let res=await mongoose.connect(process.env.MONGO_URI)
     if(res){
         console.log("mongoDB connected")
     }
