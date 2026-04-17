1069613fe1f325192ab2115b85d3974bf7a1df69
akashwaradeofficial@gmail.com <Akash Warade>
2026-04-17 10:32:21 +0530

    cookie-fixed

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,15 @@ const cookieParser=require("cookie-parser")
 
 const app=express()
 connectDB()
+app.use(cors({
+    origin:"https://auth-task-duqb.vercel.app",
+    credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.set("view engine", "ejs")
-app.use(cors({
-    origin:"https://auth-task-duqb.vercel.app",
-    credentials:true
-}))
 app.use(express.urlencoded({extended:true}))
 //phle parser rakhna  or call karna baad me routes
 app.use("/api/auth",authRoutes)
